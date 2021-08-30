9ed721c4028ac01257fca4baa8ed4fcd2245527e
Carlos && Ponce <srtager555@gmail.com>
2021-08-29 23:00:16 -0600

    create css of home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,13 @@
+import "homeCSS/style.css"
+
 export default function Home() {
     return(
-        <p>Hola</p>
+        <div className="container">
+            <div className="home-bg"></div>
+            <div className="time">
+                <p>17, 04 : 56</p>
+                <p>Vie, 27 agos. 2021</p>
+            </div>
+        </div>
     )
 }
