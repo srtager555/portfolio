5d0529bf4248dc9f17846879c3ec4c63fe38e15b
Carlos && Ponce <srtager555@gmail.com>
2021-08-29 22:45:32 -0600

    nav first structure ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from "react"
+import Home from './pages/home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="nav">
+        <button class="nav" type="button">
+          <div className="menu">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </button>
+        <div className="title">
+          <h1>Carlos Ponce</h1>
+        </div>
+        <button className="nav" type="button">
+          <div className="menu">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </button>
+      </div>
+      <div className="lineNav-1"></div>
+      <div className="lineNav-2"></div>
+      <div className="lineNav-3"></div>
+
+      <div className="body_information">
+         <Home comnponent={Home}/>
+      </div>
+    </React.Fragment>
   );
 }
 
