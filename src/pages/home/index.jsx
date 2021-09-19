b4b2e991e1d949928500571afbb695a81d16c1c3
Carlos && Ponce <srtager555@gmail.com>
2021-09-19 03:10:18 -0600

    All design versions are ready(home and nav)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,20 +5,22 @@ export const Home = () => {
     return(
         <Fragment>
             <div className="home-bg"></div>
-            <div className="container__route container__route-title">
-                <span>
-                    I'm a Front-End developer
-                </span>    
-            </div>
-            <button className="container__route container__route-show">
-                <span>
-                    Who are you?
-                </span>    
-            </button>
-            <div className="container__route container__route-name">
-                <span>
-                    Carlos Ponce
-                </span>    
+            <div className="container__home">
+                <div className="container__route container__route-title">
+                    <span>
+                        I'm a Front-End developer
+                    </span>    
+                </div>
+                <button className="container__route container__route-show">
+                    <span>
+                        Who are you?
+                    </span>    
+                </button>
+                <div className="container__route container__route-name">
+                    <span>
+                        Carlos Ponce
+                    </span>    
+                </div>
             </div>
         </Fragment>
     )
