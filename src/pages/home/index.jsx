d5c64819b1ee6d802146a8677cf7988d9e7e8c3a
Carlos && Ponce <srtager555@gmail.com>
2021-09-18 23:49:00 -0600

    creando las seccion de home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,27 +1,25 @@
-import React from "react" 
+import React, {Fragment} from "react" 
 import "./homeCSS/style.css"
 
 export const Home = () => {
     return(
-        <div style={{
-            marginTop: '100px',
-        }}>
+        <Fragment>
             <div className="home-bg"></div>
-            <div className="container container-title">
+            <div className="container__route container__route-title">
                 <span>
                     I'm a Front-End developer
                 </span>    
             </div>
-            <button className="container container-show">
+            <button className="container__route container__route-show">
                 <span>
                     Who are you?
                 </span>    
             </button>
-            <div className="container container-name">
+            <div className="container__route container__route-name">
                 <span>
                     Carlos Ponce
                 </span>    
             </div>
-        </div>
+        </Fragment>
     )
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,13 +2,16 @@ import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Layout } from "../layout";
 import { Home } from "../pages/home";
+import "./css/index.css"
 
 export const App = () => {
    return (
        <Fragment>
             <BrowserRouter>
                 <Layout /> 
-                <Route path='/' component={Home} />
+                <div className="container-master">
+                     <Route path='/' component={Home} />
+                </div>
             </BrowserRouter>
        </Fragment>
     )
